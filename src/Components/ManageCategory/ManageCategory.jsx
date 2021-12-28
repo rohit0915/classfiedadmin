efe9e77ed1b6f23fad2b875d5c7e87de5b122464
ujjwalsainiT <ujjwalreactjsdeveloper@gmail.com>
2021-12-28 15:17:36 -0800

    ujjwal

--- src/Components/ManageCategory/ManageCategory.jsx
+++ src/Components/ManageCategory/ManageCategory.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import HOC from "../../Common/HOC.jsx"
 import "./Category.css";
 import Expand from 'react-expand-animated';
 
 //material ui data table
-import { Card, Grid, Button } from "@material-ui/core";
+import { Card, Grid, Button, Dialog, DialogActions, DialogTitle, DialogContent } from "@material-ui/core";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -16,14 +16,63 @@ import Paper from "@material-ui/core/Paper";
 import TablePagination from "@material-ui/core/TablePagination";
 
 import { useNavigate } from 'react-router-dom';
+
+//for API Call
+import { getBaseUrl } from "../../utils";
+import axios from "axios";
+import Loder from "../../Loder/Loder";
+import { blankValidator, emailValidator, showNotificationMsz } from "../../utils/Validation";
+
 function ManageCategory(props) {
+    const [categoryname, setcategoryname] = useState("");
+    const [categoryimage, setcategoryimage] = useState(null)
+    const [CategoryDataArr, setCategoryDataArr] = useState([])
+    const [isloading, setisloading] = useState(false)
+    const [isUpdated, setisUpdated] = useState(false)
+
+    const [EditDailogOpen, setEditDailogOpen] = useState(false)
+    const [EditCategoryName, setEditCategoryName] = useState("")
+    const [EditCategoryImage, setEditCategoryImage] = useState("")
+    const [EditID, setEditID] = useState("")
+
+    //errors
+    const [categoryNameError, setcategoryNameError] = useState(false)
+    const [CategoryImageError, setCategoryImageError] = useState(false)
 
     const navigate = useNavigate();
     const classes = useStyles();
 
-    const filterData = [{
-        name: "ujjawal"
-    }];
+    useEffect(() => {
+        window.scrollTo(0, 0);
+
+        const getUserDetails = () => {
+            try {
+                setisloading(true);
+                let url = getBaseUrl() + "api/v1/category/getallcategory";
+                axios.get(url, {
+                    headers: {
+                        token: `Bearer ${localStorage.getItem("accessToken")}`
+                    }
+                }).then(
+                    (res) => {
+                        console.log("res::", res);
+                        setCategoryDataArr(res.data.data)
+                        setisloading(false);
+                    },
+                    (error) => {
+                        showNotificationMsz(`${error}`, "danger");
+                        console.log("data response error:::", error);
+                        setisloading(false);
+                    }
+                );
+            } catch (error) {
+                showNotificationMsz(`${error}`, "danger");
+                setisloading(false);
+                console.log("data response error:::", error);
+            }
+        };
+        getUserDetails();
+    }, [isUpdated])
 
     // for pagination hadler 
     const [rowsPerPage, setRowsPerPage] = useState(5);
@@ -41,6 +90,122 @@ function ManageCategory(props) {
         setPage(0);
     };
 
+    const AddCategorydata = () => {
+        try {
+            if (!blankValidator(categoryname)) {
+                setcategoryNameError(true);
+                return
+            }
+            if (!blankValidator(categoryimage)) {
+                setCategoryImageError(true);
+                return
+            }
+            setisloading(true)
+            let url = getBaseUrl() + "api/v1/category/addcategory";
+
+            let fd = new FormData();
+            fd.append("categoryname", categoryname);
+            fd.append("categoryimage", categoryimage);
+            axios
+                .post(url, fd)
+                .then(
+                    (res) => {
+                        console.log("add res", res)
+                        showNotificationMsz(res.data.message, "success")
+                        setisUpdated(!isUpdated)
+                        setAddCategory(!AddCategory)
+                        setcategoryname("");
+                        setcategoryimage(null)
+                        setisloading(false)
+                    },
+                    (error) => {
+                        showNotificationMsz(`${error}`, "danger")
+                        console.log("data response error:::", error)
+                        setisloading(false)
+                    }
+                )
+        } catch (error) {
+            showNotificationMsz(`${error}`, "danger")
+            setisloading(false)
+            console.log("data response error:::", error)
+        }
+
+    }
+
+    //to delete Category
+    const DeleteCategory = (ID) => {
+        //delete Id
+        let id = ID
+
+        try {
+            setisloading(true)
+            let url = getBaseUrl() + `api/v1/category/deletecategory/${id}`;
+            axios
+                .delete(url)
+                .then(
+                    (res) => {
+                        console.log("delete res", res)
+                        showNotificationMsz(res.data.message, "success")
+                        setisUpdated(!isUpdated)
+                        setisloading(false)
+                    },
+                    (error) => {
+                        showNotificationMsz(`${error}`, "danger")
+                        console.log("data response error:::", error)
+                        setisloading(false)
+                    }
+                )
+        } catch (error) {
+            showNotificationMsz(`${error}`, "danger")
+            setisloading(false)
+            console.log("data response error:::", error)
+        }
+    }
+
+    //set Edit category data
+    const EditCategoryData = (data) => {
+        setEditCategoryName(data.categoryname)
+        setEditCategoryImage(data.categoryimage)
+        setEditID(data._id)
+        setEditDailogOpen(!EditDailogOpen)
+    }
+
+
+
+    const HandleEditCategoryData = (ID) => {
+        let id = ID
+        try {
+            setisloading(true)
+            let url = getBaseUrl() + `api/v1/category/editcategory/${id}`;
+
+            let fd = new FormData();
+            fd.append("categoryname", EditCategoryName);
+            fd.append("categoryimage", EditCategoryImage);
+            axios
+                .put(url, fd)
+                .then(
+                    (res) => {
+                        console.log("add res", res)
+                        showNotificationMsz(res.data.message, "success")
+                        setisUpdated(!isUpdated)
+                        setEditDailogOpen(!EditDailogOpen)
+                        setEditCategoryName("");
+                        setEditCategoryImage(null)
+                        setisloading(false)
+                    },
+                    (error) => {
+                        showNotificationMsz(`${error}`, "danger")
+                        console.log("data response error:::", error)
+                        setisloading(false)
+                    }
+                )
+        } catch (error) {
+            showNotificationMsz(`${error}`, "danger")
+            setisloading(false)
+            console.log("data response error:::", error)
+        }
+
+    }
     return (
         <>
             <div className="Page_width content_padding" >
@@ -61,7 +226,15 @@ function ManageCategory(props) {
                                     type="text"
                                     placeholder="Enter Category Name"
                                     className="form-control"
+                                    value={categoryname}
+                                    onChange={(e) => {
+                                        setcategoryNameError(false)
+                                        setcategoryname(e.target.value)
+                                    }}
                                 />
+                                {categoryNameError && (
+                                    <span className="text-danger">Enter Category Name</span>
+                                )}
                             </div>
 
                             <div className='text_feild_heading mt-2'>Image</div>
@@ -69,16 +242,23 @@ function ManageCategory(props) {
                                 <input
                                     type="file"
                                     className="form-control"
+                                    onChange={(e) => {
+                                        setCategoryImageError(false)
+                                        setcategoryimage(e.target.files[0])
+                                    }}
                                 />
+                                {CategoryImageError && (
+                                    <span className="text-danger">Choose the Category image</span>
+                                )}
                             </div>
 
                             <div className='mt-2 mb-2'>
-                                <Button className='add_button'>Add</Button>
+                                <Button className='add_button' onClick={AddCategorydata}>Add</Button>
                             </div>
                         </Card>
                     </Expand>
                     <div className="mb-3">
-                        <Card className="p-2 Card_shadow mt-2 card_height">
+                        <Card className="p-2 Card_shadow mt-2">
 
                             <Grid className="Component_main_grid">
                                 <Grid item md={3} className="p-2">
@@ -102,6 +282,13 @@ function ManageCategory(props) {
                                                 align="left"
                                                 className="table_header"
                                             >
+                                                Category Image
+                                            </StyledTableCell>
+
+                                            <StyledTableCell
+                                                align="left"
+                                                className="table_header"
+                                            >
                                                 Name
                                             </StyledTableCell>
 
@@ -115,29 +302,34 @@ function ManageCategory(props) {
                                     </TableHead>
                                     <TableBody>
                                         {(rowsPerPage > 0
-                                            ? filterData.slice(
+                                            ? CategoryDataArr.slice(
                                                 page * rowsPerPage,
                                                 page * rowsPerPage +
                                                 rowsPerPage
                                             )
-                                            : filterData
+                                            : CategoryDataArr
                                         ).map((row) => (
                                             <StyledTableRow>
+                                                <StyledTableCell
+                                                    align="left"
+                                                >
+                                                    <img src={getBaseUrl() + row.categoryimage} alt="" style={{ height: "50px", width: "50px" }} />
+                                                </StyledTableCell>
 
                                                 <StyledTableCell
                                                     align="left"
                                                 >
-                                                    {row.name}
+                                                    {row.categoryname}
                                                 </StyledTableCell>
                                                 <StyledTableCell
                                                     align="left"
                                                 >
                                                     <div>
                                                         <span className='text-info hover_cursor'>
-                                                            <i className='fa fa-edit' />
+                                                            <i className='fa fa-edit' onClick={() => EditCategoryData(row)} />
                                                         </span>
                                                         <span className='ml-5 text-info hover_cursor'>
-                                                            <i className='fa fa-trash' />
+                                                            <i className='fa fa-trash' onClick={() => DeleteCategory(row._id)} />
                                                         </span>
 
                                                         <span className='ml-5 text-info hover_cursor' onClick={() => navigate("/manage-sub-category")}>
@@ -154,7 +346,7 @@ function ManageCategory(props) {
                                     true
                                     rowsPerPageOptions={false}
                                     component="div"
-                                    count={filterData.length}
+                                    count={CategoryDataArr.length}
                                     rowsPerPage={rowsPerPage}
                                     page={page}
                                     onChangePage={handleChangePage}
@@ -168,6 +360,66 @@ function ManageCategory(props) {
                 </div>
 
             </div>
+
+            <Dialog
+                open={EditDailogOpen}
+                aria-labelledby="form-dialog-title"
+                maxWidth="sm"
+                fullWidth="fullWidth"
+            >
+                <DialogTitle>
+                    Edit Category Data
+                    <span
+                        className="float-right icon_color"
+                        onClick={() => setEditDailogOpen(!EditDailogOpen)}
+                    >
+                        <i class="fa fa-times hover_cursor" aria-hidden="true"></i>{" "}
+                    </span>
+                </DialogTitle>
+                <DialogContent>
+
+                    <div className='text_feild_heading'>Enter Category</div>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Enter Category Name"
+                            className="form-control"
+                            value={EditCategoryName}
+                            onChange={(e) => {
+                                setEditCategoryName(e.target.value)
+                            }}
+                        />
+                    </div>
+
+                    <div className='text_feild_heading mt-2'>Image</div>
+                    <div>
+                        <input
+                            type="file"
+                            className="form-control"
+                            onChange={(e) => {
+                                setEditCategoryImage(e.target.files[0])
+                            }}
+                        />
+                    </div>
+
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        className="button_formatting"
+                        onClick={() => setEditDailogOpen(!EditDailogOpen)}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        className="button_formatting"
+                        onClick={() => HandleEditCategoryData(EditID)}
+                    >
+                        Save{" "}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Loder loading={isloading} />
         </>
     )
 }
